c5e1f91753507d092b211ca16ec936c4badab114
Gregor Weiss <gregor.weiss@hlrs.de>
2024-12-12 17:52:01 +0100

    add lookup tables with slight mods

--- public_header.py
+++ public_header.py
@@ -93,7 +93,7 @@ if os.path.isdir(destination) == False:
 
 include_files = ['macros.h', 'constants.h', 'dwt.h', 'tagtree.h', 'mq_types.h', 'mq.h',
                 'bitstream.h', 'codestream.h', 'ht_block_encoding.h', 'tier2.h', 'types.h', 'libbwc.h']
-exclude_files = ["prim_types_double.h", "prim_types_single.h", "tier1.h"]
+exclude_files = ["prim_types_double.h", "prim_types_single.h", "tier1.h", "enc_CxtVLC_tables.h"]
 all_files = [f for f in os.listdir(source) if os.path.isfile(os.path.join(source, f))]
 missing_files = [f for f in all_files if f not in include_files and f not in exclude_files]
 if missing_files:
