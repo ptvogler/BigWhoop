d23fd794af3fe9bdd3261e7e18152be6493d0914
Gregor Weiss <gregor.weiss@hlrs.de>
2025-04-02 11:04:33 +0200

    clang-format

--- tests/test_libbwc.cpp
+++ tests/test_libbwc.cpp
@@ -48,31 +48,33 @@
 #include <cstdint>
 
 #ifdef __cplusplus
-  extern "C" {
+extern "C"
+{
 #endif
 #include "../include/library/private/libbwc.h"
 #ifdef __cplusplus
-  }
+}
 #endif
 
-TEST_CASE( "Stream initialization", "[bwc_init_stream]" ) {
-    constexpr uint32 size = 4;
-    uchar inp_mem[size];
-    uchar out_mem[size];
-    out_mem[3] = inp_mem[0] = 1;
-    out_mem[2] = inp_mem[1] = 2;
-    out_mem[1] = inp_mem[2] = 3;
-    out_mem[0] = inp_mem[3] = 4;
+TEST_CASE ("Stream initialization", "[bwc_init_stream]")
+{
+  constexpr uint32 size = 4;
+  uchar  inp_mem[size];
+  uchar  out_mem[size];
+  out_mem[3] = inp_mem[0] = 1;
+  out_mem[2] = inp_mem[1] = 2;
+  out_mem[1] = inp_mem[2] = 3;
+  out_mem[0] = inp_mem[3] = 4;
 
-    bwc_stream *stream = bwc_init_stream(inp_mem, out_mem, comp);
+  bwc_stream *stream = bwc_init_stream (inp_mem, out_mem, comp);
 
-    REQUIRE(((uchar*)stream->inp)[0] == 1);
-    REQUIRE(((uchar*)stream->inp)[1] == 2);
-    REQUIRE(((uchar*)stream->inp)[2] == 3);
-    REQUIRE(((uchar*)stream->inp)[3] == 4);
+  REQUIRE (((uchar *)stream->inp)[0] == 1);
+  REQUIRE (((uchar *)stream->inp)[1] == 2);
+  REQUIRE (((uchar *)stream->inp)[2] == 3);
+  REQUIRE (((uchar *)stream->inp)[3] == 4);
 
-    REQUIRE(((uchar*)stream->out)[0] == 4);
-    REQUIRE(((uchar*)stream->out)[1] == 3);
-    REQUIRE(((uchar*)stream->out)[2] == 2);
-    REQUIRE(((uchar*)stream->out)[3] == 1);
+  REQUIRE (((uchar *)stream->out)[0] == 4);
+  REQUIRE (((uchar *)stream->out)[1] == 3);
+  REQUIRE (((uchar *)stream->out)[2] == 2);
+  REQUIRE (((uchar *)stream->out)[3] == 1);
 }
